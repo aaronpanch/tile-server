77b5fa5d5f940f7c560ee7d6e70512eca55535b4
Aaron Panchal <aaron.panchal@appfolio.com>
2016-07-23 02:11:16 -0700

    Add mongodb client and connect to DB

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const koa = require('koa')
-    , route = require('koa-route');
+    , co = require('co')
+    , route = require('koa-route')
+    , MongoClient = require('mongodb').MongoClient;
+
 const app = koa();
 
 app.use(require('koa-logger')());
@@ -8,5 +11,10 @@ app.use(route.get('/', function *() {
   this.body = 'hi!'
 }));
 
-app.listen(3000);
-console.log('Tile Server listening on port 3000');
+co(function *() {
+  const db = yield MongoClient.connect('mongodb://localhost:27017/vacancies-dev');
+  app.context.db = db;
+
+  app.listen(3000);
+  console.log('Tile Server listening on port 3000');
+}).catch(err => { console.log(err); });
